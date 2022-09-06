5d9498c191b2f56ea6aa192976ecc47b476cfc59
Francisco <fran.perez.c@hotmail.com>
2022-09-06 02:08:26 -0300

    mend

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
 export default {
-  server: {
-    proxy: {
-      '/calc-generator': {
-        target: 'https://fpece.com',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/calc-generator/, '')
-      }
-    }
+  build: {
+    base: 'https://fpece.com/calc-generator/'
   }
 }
