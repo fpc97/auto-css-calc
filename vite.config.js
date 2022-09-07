86a0d844cbd39cba24070a2177c0f28d3db565a4
Francisco <fran.perez.c@hotmail.com>
2022-09-07 18:06:26 -0300

    configure pwa

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 
 export default defineConfig({
-  base: '/calc-generator/'
+  base: '/calc-generator/',
+  plugins: [
+    VitePWA({ registerType: 'autoUpdate' })
+  ]
 })
